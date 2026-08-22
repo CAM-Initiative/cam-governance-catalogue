691355bdad67aeaacbd5a57acfb28b819ed940bd
Aeon Governance Lab <aeon.tier@protonmail.com>
2026-08-22 21:46:01 +0800

    fix: load shared VIGIL reading legibility layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,6 @@ import App from "./App";
 import "./index.css";
 import "./dark-appearance.css";
 import "./home-ux.css";
-import "./case-file-legibility.css";
+import "./vigil-reading-legibility.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
