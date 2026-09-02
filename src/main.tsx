e6a067795158187adf33cd81dcb8757b2bb650be
Michelle O'Rourke <aeon.tier@protonmail.com>
2026-09-02 18:55:45 +0800

    Load Incident severity UX refinements

--- src/main.tsx
+++ src/main.tsx
@@ -20,6 +20,7 @@ import "./sitewide-typography-floor.css";
 import "./vigil-evidence-reading-hierarchy.css";
 import "./vigil-case-file-dossier.css";
 import "./vigil-case-file-polish.css";
+import "./vigil-incident-severity-refinement.css";
 import "./vigil-case-library-simplify.css";
 import "./vigil-observatory-surface-cleanup.css";
 import "./governance-rail-refinements.css";
