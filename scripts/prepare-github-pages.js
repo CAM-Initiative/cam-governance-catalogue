a4ac1d056591cac38ceabf25961d10373aaad82c
Michelle O'Rourke <aeon.tier@protonmail.com>
2026-09-14 19:40:38 +0800

    fix: remove synthetic sitemap lastmod

--- scripts/prepare-github-pages.js
+++ scripts/prepare-github-pages.js
@@ -318,7 +318,6 @@ const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${sitemapRoutes.map((route) => `  <url>
     <loc>${routeUrl(route)}</loc>
-    <lastmod>${generatedDate}</lastmod>
   </url>`).join("\n")}
 </urlset>
 `;
