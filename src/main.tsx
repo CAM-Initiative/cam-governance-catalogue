3511b06bfa20b9b3b389c716567f2c58098203fe
Michelle O'Rourke <aeon.tier@protonmail.com>
2026-09-17 20:31:18 +0800

    ux: load mixed-execution exemplar styles

--- src/main.tsx
+++ src/main.tsx
@@ -20,6 +20,7 @@ import "./sitewide-typography-floor.css";
 import "./vigil-evidence-reading-hierarchy.css";
 import "./vigil-case-file-dossier.css";
 import "./vigil-case-file-polish.css";
+import "./vigil-exemplar-mixed-execution.css";
 import "./vigil-incident-severity-refinement.css";
 import "./vigil-case-library-simplify.css";
 import "./vigil-observatory-surface-cleanup.css";
