a15bc0339006deffd0cf8a0e9bd75ed1aa535712
Michelle O'Rourke <aeon.tier@protonmail.com>
2026-09-17 21:38:55 +0800

    Load Case File assessment provenance cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -23,6 +23,7 @@ import "./vigil-case-file-polish.css";
 import "./vigil-exemplar-mixed-execution.css";
 import "./vigil-incident-severity-refinement.css";
 import "./vigil-harm-assessment-cleanup.css";
+import "./vigil-assessment-provenance-cleanup.css";
 import "./vigil-case-library-simplify.css";
 import "./vigil-observatory-surface-cleanup.css";
 import "./governance-rail-refinements.css";
