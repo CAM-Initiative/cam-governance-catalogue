ec08f634adb6dfcbfdbb67635596061e4a741b6a
Michelle O'Rourke <aeon.tier@protonmail.com>
2026-09-22 21:38:53 +0800

    Load assessment hierarchy refinements

--- src/main.tsx
+++ src/main.tsx
@@ -24,6 +24,7 @@ import "./vigil-exemplar-mixed-execution.css";
 import "./vigil-incident-severity-refinement.css";
 import "./vigil-harm-assessment-cleanup.css";
 import "./vigil-assessment-provenance-cleanup.css";
+import "./vigil-assessment-layout-v2.css";
 import "./vigil-classification-table.css";
 import "./vigil-repair-web-cleanup.css";
 import "./vigil-case-library-simplify.css";
