ca98ed7865009cd619d38405b6623702e8a6d55c
Michelle O'Rourke <aeon.tier@protonmail.com>
2026-09-23 00:49:27 +0800

    Load final homepage cog depth and explorer order refinements

--- src/main.tsx
+++ src/main.tsx
@@ -40,5 +40,6 @@ import "./home-premium-v4.css";
 import "./home-premium-v5.css";
 import "./home-premium-v6.css";
 import "./home-premium-v7.css";
+import "./home-premium-v8.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
