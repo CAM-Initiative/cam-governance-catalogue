df5fe78bc4c427030335e24ebc002e4e39cf7e75
Michelle O'Rourke <aeon.tier@protonmail.com>
2026-09-23 00:02:15 +0800

    Load taxonomy reveal styling

--- src/main.tsx
+++ src/main.tsx
@@ -38,5 +38,6 @@ import "./about-page-polish.css";
 import "./public-surface-responsive-cleanup.css";
 import "./home-premium-v4.css";
 import "./home-premium-v5.css";
+import "./home-premium-v6.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
